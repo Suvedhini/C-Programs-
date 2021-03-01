100181baa15357585777884e3d43d7777a3da4ad
Suvedhini Reddiyar <47688060+Suvedhini@users.noreply.github.com>
2021-03-01 18:20:28 +0530

    Create cout.cpp

---
+++ cout.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=3;
+    float y=6.7;
+    char z='a';
+    cout<<"Value of x(int) is :"<<x<<endl;
+    cout<<"Value of y(float) is :"<<y<<endl;
+    cout<<"Value of z(char) is: "<<z<<endl;
+return 0;
+}
