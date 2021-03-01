0457d6d4fd5838889041ff382ee54b44ef333869
Suvedhini Reddiyar <47688060+Suvedhini@users.noreply.github.com>
2021-03-01 18:29:49 +0530

    Create implicit_conversion.cpp

---
+++ implicit_conversion.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num_int=15;
+    double num_double;
+    num_double=num_int;
+    cout<<"Implicit conversion "<<endl;
+    cout<<"Value of num_int is: "<<num_int<<endl;
+    cout<<"Value of num_double is: "<<num_double<<endl;
+return 0;
+}
