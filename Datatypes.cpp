0c95776c7c345244bfae3b4def8cf9fb88615ccf
Suvedhini Reddiyar <47688060+Suvedhini@users.noreply.github.com>
2021-05-14 19:40:31 +0530

    Create Datatypes.cpp

---
+++ Datatypes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    a=7;
+    cout<<"The value of a is:"<<sizeof(a)<<"\n";
+
+    float b=9.8;
+    cout<<"The value of b is:"<<sizeof(b)<<"\n";
+
+    char c;
+    cout<<"The value of c is:"<<sizeof(c)<<"\n";
+
+    bool d;
+    cout<<"The value of d is:"<<sizeof(d)<<"\n";
+
+    return 0;
+}
